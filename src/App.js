df13d7daf9bb5737221c40517222daf603acdd28
CodeSandbox <support@codesandbox.io>
2023-12-28 19:24:12 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./styles.css";
+import Login from "./components/login";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Login />;
 }
